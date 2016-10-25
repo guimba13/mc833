71247487f982ffe7d9a5f1797c20022390caa3ca
wilson.martins <martins.wilson17@gmail.com>
2016-10-24 23:22:06 -0200

    Identando e adicionando encerramento da conexao atraves do cliente
    Fechar a conexao do cliente/servidor usando os comandos quit, exit ou close

--- lab4/cliente.c
+++ lab4/cliente.c
@@ -13,85 +13,91 @@
 #define MAXLINE 4096
 
 int main(int argc, char **argv) {
-    int    sockfd, n;
-    char   recvline[MAXLINE + 1];
-    char   error[MAXLINE + 1];
-    char   msg[1000];
-    struct sockaddr_in servaddr, _local, _remote;
+  int    sockfd, n;
+  char   recvline[MAXLINE + 1];
+  char   error[MAXLINE + 1];
+  char   msg[1000];
+  struct sockaddr_in servaddr, _local, _remote;
 
-    // Verifica o argumento passada para o cliente pela entrada padrao
-    if (argc != 2) {
-        strcpy(error,"uso: ");
-        strcat(error,argv[0]);
-        strcat(error," <IPaddress>");
-        perror(error);
+  // Verifica o argumento passada para o cliente pela entrada padrao
+  if (argc != 2) {
+    strcpy(error,"uso: ");
+    strcat(error,argv[0]);
+    strcat(error," <IPaddress>");
+    perror(error);
+    exit(1);
+  }
+
+  // Pega o socket para ser realizado a conexao
+  if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket error");
+    exit(1);
+  }
+
+  // Inicializa a variaevel de endereço do servidor
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port   = htons(13000);
+  // Verifica a validade do IP passado como parametro
+  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
+    perror("inet_pton error");
+    exit(1);
+  }
+
+  // Realiza a conexao com o servidor
+  if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+    perror("connect error");
+    exit(1);
+  }
+
+  // Chama a função getsockname para pegar o endereço ip e porta do socket local
+  bzero(&_local, sizeof(_local));
+  socklen_t len = sizeof(_local);
+  if (getsockname (sockfd, (struct sockaddr *) &_local, &len) == -1) {
+    perror("getsockname() failed");
+    return -1;
+  }
+
+  //Imprime as informações do socket local
+  printf("IP SOCKET LOCAL: %s\nPORT SOCKET LOCAL: %d\n", inet_ntoa(_local.sin_addr), ntohs(_local.sin_port));
+
+  //Pega as informações do socket remoto
+  socklen_t lenRemote = sizeof(_remote);
+  if (getpeername(sockfd, (struct sockaddr *) &_remote, &lenRemote) == -1) {
+    perror("getpeername() failed hard");
+    return -1;
+  }
+  //Imprime as informações do socket remoto
+  printf("\nIP SOCKET REMOTO: %s\nPORT SOCKET REMOTO: %d\n", inet_ntoa(_remote.sin_addr), ntohs(_remote.sin_port));
+
+  // Troca de mensagens com o servidor
+  do {
+    recvline[n] = 0;
+    // Le a entrada padrao
+    fgets(msg, sizeof(msg), stdin);
+    if (strcmp("exit\n", msg) == 0 || strcmp("quit\n", msg) == 0
+      || strcmp("close\n", msg) == 0) {
+      close(sockfd);
+      exit(0);
+    } else {
+      if (send(sockfd, msg, strlen(msg), 0) < 0) {
+        puts("Send failed.");
+        return 1;
+      }
+      // Escreve na saida padrao a informacao obtida do servidor
+      n = read(sockfd, recvline, MAXLINE);
+      if (fputs(recvline, stdout) == EOF) {
+        perror("fputs error");
         exit(1);
+      }
     }
+    fflush(stdin);
+  } while (1);
+  // Verifica por erro na conexao
+  if (n < 0) {
+    perror("read error");
+    exit(1);
+  }
 
-    // Pega o socket para ser realizado a conexao
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket error");
-        exit(1);
-    }
-
-    // Inicializa a variaevel de endereço do servidor
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port   = htons(13000);
-    // Verifica a validade do IP passado como parametro
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        perror("inet_pton error");
-        exit(1);
-    }
-
-    // Realiza a conexao com o servidor
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        perror("connect error");
-        exit(1);
-    }
-
-    // Chama a função getsockname para pegar o endereço ip e porta do socket local
-    bzero(&_local, sizeof(_local));
-    socklen_t len = sizeof(_local);
-    if (getsockname (sockfd, (struct sockaddr *) &_local, &len) == -1) {
-        perror("getsockname() failed");
-        return -1;
-    }
-
-    //Imprime as informações do socket local
-    printf("IP SOCKET LOCAL: %s\nPORT SOCKET LOCAL: %d\n", inet_ntoa(_local.sin_addr), ntohs(_local.sin_port));
-
-    //Pega as informações do socket remoto
-    socklen_t lenRemote = sizeof(_remote);
-    if (getpeername(sockfd, (struct sockaddr *) &_remote, &lenRemote) == -1) {
-        perror("getpeername() failed hard");
-        return -1;
-    }
-    //Imprime as informações do socket remoto
-    printf("\nIP SOCKET REMOTO: %s\nPORT SOCKET REMOTO: %d\n", inet_ntoa(_remote.sin_addr), ntohs(_remote.sin_port));
-
-    // Troca de mensagens com o servidor
-    do {
-        recvline[n] = 0;
-        // Le a entrada padrao
-        fgets(msg, sizeof(msg), stdin);
-        if (send(sockfd, msg, strlen(msg), 0) < 0) {
-          puts("Send failed.");
-          return 1;
-        }
-        // Escreve na saida padrao a informacao obtida do servidor
-        n = read(sockfd, recvline, MAXLINE);
-        if (fputs(recvline, stdout) == EOF) {
-            perror("fputs error");
-            exit(1);
-        }
-        fflush(stdin);
-    } while (1);
-    // Verifica por erro na conexao
-    if (n < 0) {
-        perror("read error");
-        exit(1);
-    }
-
-    exit(0);
+  exit(0);
 }
--- lab4/servidor.c
+++ lab4/servidor.c
@@ -17,89 +17,89 @@
 void* connection_handler(void* socket_desc);
 
 int main (int argc, char **argv) {
-   int    listenfd, connfd, *new_sock;
-   struct sockaddr_in servaddr,_self;
-   char   buf[MAXDATASIZE];
-   char   str[INET_ADDRSTRLEN];
-   time_t ticks;
+  int    listenfd, connfd, *new_sock;
+  struct sockaddr_in servaddr,_self;
+  char   buf[MAXDATASIZE];
+  char   str[INET_ADDRSTRLEN];
+  time_t ticks;
 
   // Começa a escutar o socket para a conexao
-   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-      perror("socket");
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
+
+  // Inicializa a variavel de endereco
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family      = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port        = htons(13000);
+
+  // Da bind na porta e no socket e trata o erro
+  if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+    perror("bind");
+    exit(1);
+  }
+
+  // Escuta o socket da conexao e trata o erro
+  if (listen(listenfd, LISTENQ) == -1) {
+    perror("listen");
+    exit(1);
+  }
+
+  // Mantem o servidor rodando infinitamente
+  for ( ; ; ) {
+    // Aceita a conexao de um cliente e trata o erro
+    socklen_t len = sizeof(_self);
+    if ((connfd = accept(listenfd, (struct sockaddr *) &_self, &len)) == -1 ) {
+      perror("accept");
       exit(1);
-   }
+    }
+    inet_ntop(AF_INET, &(_self.sin_addr), str, INET_ADDRSTRLEN);
+    //chama a função getpeername para pegar o endereço ip e porta do socket remoto
+    printf("IP SOCKET REMOTO: %s\nPORT SOCKET REMOTO: %d\n", str, ntohs(_self.sin_port));
 
-	// Inicializa a variavel de endereco
-   bzero(&servaddr, sizeof(servaddr));
-   servaddr.sin_family      = AF_INET;
-   servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   servaddr.sin_port        = htons(13000);
-
-	// Da bind na porta e no socket e trata o erro
-   if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
-      perror("bind");
+    //cria uma thread para cada conexão
+    pthread_t thread;
+    new_sock = malloc(1);
+    *new_sock = connfd;
+    if (pthread_create(&thread, NULL, connection_handler, (void*) new_sock) < 0) {
+      perror("Could not create thread");
       exit(1);
-   }
-
-	// Escuta o socket da conexao e trata o erro
-   if (listen(listenfd, LISTENQ) == -1) {
-      perror("listen");
-      exit(1);
-   }
-
-	// Mantem o servidor rodando infinitamente
-   for ( ; ; ) {
-	   // Aceita a conexao de um cliente e trata o erro
-      socklen_t len = sizeof(_self);
-      if ((connfd = accept(listenfd, (struct sockaddr *) &_self, &len)) == -1 ) {
-         perror("accept");
-         exit(1);
-      }
-      inet_ntop(AF_INET, &(_self.sin_addr), str, INET_ADDRSTRLEN);
-      //chama a função getpeername para pegar o endereço ip e porta do socket remoto
-      printf("IP SOCKET REMOTO: %s\nPORT SOCKET REMOTO: %d\n", str, ntohs(_self.sin_port));
-
-      //cria uma thread para cada conexão
-      pthread_t thread;
-      new_sock = malloc(1);
-      *new_sock = connfd;
-      if (pthread_create(&thread, NULL, connection_handler, (void*) new_sock) < 0) {
-		      perror("Could not create thread");
-		      exit(1);
-      }
-   }
-   return(0);
+    }
+  }
+  return(0);
 }
 
 //handler utilizado para a thread
 void *connection_handler(void* socket_desc) {
-	int sock = *(int*)socket_desc, read_size;
-	char message[2000];
-	char   buf[MAXDATASIZE];
+  int sock = *(int*)socket_desc, read_size;
+  char message[2000];
+  char   buf[MAXDATASIZE];
 
   //lê constantemente do socket
-	while( (read_size = recv(sock , message , 2000 , 0)) > 0 )
-    {
-        //imprime a mensagem recebida
-        puts(message);
-        //executa o comando
-        system(message);
-        //Envia a mensagem de volta ao cliente
-        write(sock , message , strlen(message));
-    }
+  while( (read_size = recv(sock , message , 2000 , 0)) > 0 )
+  {
+    //imprime a mensagem recebida
+    puts(message);
+    //executa o comando
+    system(message);
+    //Envia a mensagem de volta ao cliente
+    write(sock , message , strlen(message));
+  }
 
   //realiza verificações
-	if(read_size == 0) {
-        puts("Client disconnected");
-        fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
+  if(read_size == 0) {
+    puts("Client disconnected");
+    fflush(stdout);
+  }
+  else if(read_size == -1)
+  {
+    perror("recv failed");
+  }
 
-	// Fecha a conexao com o servidor
-	free(socket_desc);
+  // Fecha a conexao com o servidor
+  free(socket_desc);
 
-	return 0;
+  return 0;
 }
